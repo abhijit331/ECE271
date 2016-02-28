2a1e688e33dc1412045a2c4f0a6396deebce80ee
dexter11235813 <abgairola@gmail.com>
2016-02-28 01:50:00 -0800

    expanded training set

--- final.R
+++ final.R
@@ -1,36 +1,40 @@
-"~"
+
 install.packages("tuneR")
 install.packages("randomForest")
 library(randomForest)
 library(rpart)
 library(tuneR)
 # Metal,Jazz,Classical,Pop,Electronica,Blues
-blue = readWave("blues.00000.wav")
-x = melfcc(blue)
-par(new = F)
-for( i in 1:3006)
-{
-  plot(x[i,],type="l")
-  par(new = T)
-}
 
-metal = readWave("C:/Users/abhijit331/Desktop/271B/genres_wav/metal_wav/metal.00000.wav")
-y = melfcc(metal)
-par(new = F)
-for(i in 1:3005)
+#Blues
+
+setwd("/home/abhijit331/Desktop/ECE271B/genres_wav/blues_wav")
+files = list.files(pattern = "*.wav")
+temp = readWave(files[1])
+blues = melfcc(temp)
+for(i in 2:50)
 {
-  plot(y[i,],type = "l")
-  par(new = T)
+  temp = readWave(files[i])
+  blues = rbind(blues,melfcc(temp))
 }
-x = cbind(1,x)
-y = cbind(0,y)
-train = rbind(x,y)
+blues = cbind(1,blues) 
+
+#Metal 
+setwd("/home/abhijit331/Desktop/ECE271B/genres_wav/metal_wav")
+files = list.files(pattern = "*.wav")
+temp = readWave(files[1])
+metal = melfcc(temp)
+for(i in 2:50)
+{
+  temp = readWave(files[i])
+  metal = rbind(metal,melfcc(temp))
+}
+metal = cbind(0,metal)
+train = rbind(blues,metal)
 colnames(train) = c("label","coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
 train = as.data.frame(train)
-mod = rpart(label ~ . , data = train)
 
-test =  melfcc(readWave("blues.00001.wav"))
-test = cbind(-1,test)
+test =  melfcc(readWave("blues.00051.wav"))
 colnames(test) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
 rf = randomForest(as.factor(label) ~ . , data = train)
 pre = predict(rf,test)
