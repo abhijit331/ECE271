d304604c492685fb804677e479925aa01f0a7e98
dexter11235813 <abgairola@gmail.com>
2016-02-28 03:38:37 -0800

    tested with a larger set

--- final.R
+++ final.R
@@ -12,7 +12,7 @@ setwd("/home/abhijit331/Desktop/ECE271B/genres_wav/blues_wav")
 files = list.files(pattern = "*.wav")
 temp = readWave(files[1])
 blues = melfcc(temp)
-for(i in 2:50)
+for(i in 2:40)
 {
   temp = readWave(files[i])
   blues = rbind(blues,melfcc(temp))
@@ -24,7 +24,7 @@ setwd("/home/abhijit331/Desktop/ECE271B/genres_wav/metal_wav")
 files = list.files(pattern = "*.wav")
 temp = readWave(files[1])
 metal = melfcc(temp)
-for(i in 2:50)
+for(i in 2:40)
 {
   temp = readWave(files[i])
   metal = rbind(metal,melfcc(temp))
@@ -34,7 +34,7 @@ train = rbind(blues,metal)
 colnames(train) = c("label","coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
 train = as.data.frame(train)
 
-test =  melfcc(readWave("blues.00051.wav"))
+test =  melfcc(readWave("metal.00051.wav"))
 colnames(test) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
 rf = randomForest(as.factor(label) ~ . , data = train)
 pre = predict(rf,test)
@@ -43,7 +43,12 @@ colnames(test2) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff
 pre2 = predict(rf,test2)
 pre2
 
-test3 = melfcc(readWave("metal.00007.wav"))
+# interesting = blues 61
+result =c()
+for(i in 51:100)
+{
+test3 = melfcc(readWave(files[i]))
 colnames(test3) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
 pred3 = predict(rf,test3)
-length(which(pred3 == 0))
+result[i-50] =ifelse((length(which(pred3==0)) > length(which(pred3==1))),0,1)
+}
