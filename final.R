63ebc0dae7ec5c5e347f4412a97b37824a28c207
Abhijit Gairola <Abhijit Gairola>
2016-03-01 00:00:44 -0800

    added factors

--- final.R
+++ final.R
@@ -4,51 +4,82 @@ install.packages("randomForest")
 library(randomForest)
 library(rpart)
 library(tuneR)
+library(plyr)
 # Metal,Jazz,Classical,Pop,Electronica,Blues
 
 #Blues
 
-setwd("/home/abhijit331/Desktop/ECE271B/genres_wav/blues_wav")
-files = list.files(pattern = "*.wav")
-temp = readWave(files[1])
+#setwd("/home/abhijit331/Desktop/ECE271B/genres_wav/blues_wav")
+setwd("C:/Users/abhijit331/Desktop/Math 271b/genres_wav/blues_wav")
+files.blues = list.files(pattern = "*.wav")
+temp = readWave(files.blues[1])
 blues = melfcc(temp)
-for(i in 2:40)
+for(i in 2:10)
 {
-  temp = readWave(files[i])
+  temp = readWave(files.blues[i])
   blues = rbind(blues,melfcc(temp))
 }
-blues = cbind(1,blues) 
+blues = cbind(0,blues) 
 
 #Metal 
-setwd("/home/abhijit331/Desktop/ECE271B/genres_wav/metal_wav")
-files = list.files(pattern = "*.wav")
-temp = readWave(files[1])
+#setwd("/home/abhijit331/Desktop/ECE271B/genres_wav/metal_wav")
+setwd("C:/Users/abhijit331/Desktop/Math 271b/genres_wav/metal_wav")
+files.metal = list.files(pattern = "*.wav")
+temp = readWave(files.metal[1])
 metal = melfcc(temp)
-for(i in 2:40)
+for(i in 2:10)
 {
-  temp = readWave(files[i])
+  temp = readWave(files.metal[i])
   metal = rbind(metal,melfcc(temp))
 }
-metal = cbind(0,metal)
-train = rbind(blues,metal)
+metal = cbind(1,metal)
+
+# Jazz
+setwd("C:/Users/abhijit331/Desktop/Math 271b/genres_wav/jazz_wav")
+files.jazz  = list.files(pattern = "*.wav")
+temp = readWave(files.jazz[1])
+jazz = melfcc(temp)
+for(i in 2:10)
+{
+  temp = readWave(files.jazz[i])
+  jazz = rbind(jazz,melfcc(temp))
+}
+jazz = cbind(2,jazz)
+
+# train
+train = rbind(blues,metal,jazz)
 colnames(train) = c("label","coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
 train = as.data.frame(train)
-
-test =  melfcc(readWave("metal.00051.wav"))
-colnames(test) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
 rf = randomForest(as.factor(label) ~ . , data = train)
-pre = predict(rf,test)
-test2 =  melfcc(readWave("blues.00004.wav"))
-colnames(test2) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
-pre2 = predict(rf,test2)
-pre2
 
 # interesting = blues 61
-result =c()
-for(i in 51:100)
+result.blues = c()
+result.metal = c()
+result.jazz  = c()
+for(i in 31:100)
 {
-test3 = melfcc(readWave(files[i]))
-colnames(test3) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
-pred3 = predict(rf,test3)
-result[i-50] =ifelse((length(which(pred3==0)) > length(which(pred3==1))),0,1)
+setwd("C:/Users/abhijit331/Desktop/Math 271b/genres_wav/blues_wav")
+files.blues = list.files(pattern = "*.wav")
+test.blues = melfcc(readWave(files.blues[i]))
+setwd("C:/Users/abhijit331/Desktop/Math 271b/genres_wav/metal_wav")
+files.metal = list.files(pattern = "*.wav")  
+test.metal = melfcc(readWave(files.metal[i]))
+setwd("C:/Users/abhijit331/Desktop/Math 271b/genres_wav/jazz_wav")
+files.jazz = list.files(pattern = "*.wav")
+test.jazz = melfcc(readWave(files.jazz[i]))
+colnames(test.blues) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
+colnames(test.metal) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
+colnames(test.jazz) = c("coeff1","coeff2","coeff3","coeff4","coeff5","coeff6","coeff7","coeff8","coeff9","coeff10","coeff11","coeff12")
+pred.blues = predict(rf,test.blues)
+pred.metal = predict(rf,test.metal)
+pred.jazz = predict(rf,test.jazz)
+temp =rev(order(count(pred.blues)$freq))
+result.blues[i-30] = as.integer(as.matrix(count(pred.blues))[temp[1],1])
+temp = rev(order(count(pred.metal)$freq))
+result.metal[i-30] =as.integer(as.matrix(count(pred.metal))[temp[1],1])
+temp = rev(order(count(pred.jazz)$freq))
+result.jazz[i-30] =as.integer(as.matrix(count(pred.jazz))[temp[1],1])
 }
+table(result.blues)
+table(result.metal)
+table(result.jazz)
